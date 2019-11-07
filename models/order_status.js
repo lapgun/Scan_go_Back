64a2a74cd1a6121c0ca9b4334e6e0be7bbfb29b7
Hoàng Thuận <hoangthuan19071998@gmail.com>
2019-11-07 18:03:36 +0700

    end the day

--- models/order_status.js
+++ models/order_status.js
@@ -1,10 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Order_status = sequelize.define('Order_status', {
-    name: DataTypes.STRING,
-    orderId: DataTypes.INTEGER,
-    note: DataTypes.STRING
-  }, {});
+  const Order_status = sequelize.define(
+    "Order_status",
+    {
+      name: DataTypes.STRING,
+      orderId: DataTypes.INTEGER,
+      note: DataTypes.STRING
+    },
+    {}
+  );
   Order_status.associate = function(models) {
     // associations can be defined here
   };
