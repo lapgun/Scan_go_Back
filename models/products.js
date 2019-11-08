41768d1e85bb5b5770cd23b0724c8982fec75c5e
lapgun <lapgun06@gmail.com>
2019-11-08 16:02:51 +0700

    update

--- models/products.js
+++ models/products.js
@@ -1,20 +1,20 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Products = sequelize.define(
-    "Products",
-    {
-      name: DataTypes.STRING,
-      categoriesId: DataTypes.INTEGER,
-      picture: DataTypes.STRING,
-      price: DataTypes.INTEGER,
-      description: DataTypes.STRING,
-      detail: DataTypes.STRING,
-      order_time: DataTypes.INTEGER
-    },
-    {}
-  );
-  Products.associate = function(models) {
-    // associations can be defined here
-  };
-  return Products;
+    const Products = sequelize.define(
+        "Products",
+        {
+            name: DataTypes.STRING,
+            categoriesId: DataTypes.INTEGER,
+            picture: DataTypes.STRING,
+            price: DataTypes.INTEGER,
+            description: DataTypes.STRING,
+            detail: DataTypes.STRING,
+            order_time: DataTypes.INTEGER
+        },
+        {}
+    );
+    Products.associate = function (models) {
+        // associations can be defined here
+    };
+    return Products;
 };
