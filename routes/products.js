575431977f1ec7585a6be61a7d7f47d52ada5d4d
lapgun <lapgun06@gmail.com>
2019-11-11 13:43:44 +0700

    upload file

--- routes/products.js
+++ routes/products.js
@@ -1,54 +1,56 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
-var multer  = require('multer');
+var multer = require('multer');
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'storage/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now()+'_'+ file.originalname)
-  }
+    destination: function (req, file, cb) {
+        cb(null, 'storage/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '_' + file.originalname)
+    }
 })
 
-var upload = multer({ storage: storage });
+var upload = multer({storage: storage});
 
 /* GET Products listing. */
-router.get("/", function(req, res, next) {
-  db.Products.findAll().then(results => res.send({ data: results }));
+router.get("/", function (req, res, next) {
+    db.Products.findAll().then(results => res.send({data: results}));
 });
 
 // Get by id
-router.get("/:id", function(req, res, next) {
-  db.Products.findByPk(req.params.id).then(results =>
-    res.send({ data: results })
-  );
+router.get("/:id", function (req, res, next) {
+    db.Products.findByPk(req.params.id).then(results =>
+        res.send({data: results})
+    );
 });
 
 // Post
-router.post("/create",upload.single('picture'), function(req, res, next) {
-  console.log(req.file);
-  console.log('sdsaasd'+ req.body)
-  return res.send(req.file);
-  // db.Products.create(form).then(res.send({ message: "create success" }));
-  // console.log(req.file);
+router.post("/create", upload.single('picture'), function (req, res, next) {
+    let form = req.body;
+
+    form.picture = req.file.filename;
+    db.Products.create(form).then(res.send({message: "create success"}));
+    // // res.send(req.body);
+    //  console.log(req.body);
+    //  console.log(req.file);
 });
 
 //Update
 
-router.put("/:id", function(req, res, next) {
-  let form = req.body;
-  db.Products.update(form, {
-    where: {
-      id: req.params.id
-    }
-  }).then(res.send({ message: "update success" }));
+router.put("/:id", function (req, res, next) {
+    let form = req.body;
+    db.Products.update(form, {
+        where: {
+            id: req.params.id
+        }
+    }).then(res.send({message: "update success"}));
 });
 
 //Delete
-router.delete("/:id", function(req, res, next) {
-  db.Products.destroy({ where: { id: req.params.id } }).then(
-    res.send({ message: "delete success" })
-  );
+router.delete("/:id", function (req, res, next) {
+    db.Products.destroy({where: {id: req.params.id}}).then(
+        res.send({message: "delete success"})
+    );
 });
 module.exports = router;
