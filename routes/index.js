4c1871afdd3dc9b73ba4877dfc33fd638556d532
Hoàng Thuận <hoangthuan19071998@gmail.com>
2019-11-26 14:51:23 +0700

    hello

--- routes/index.js
+++ routes/index.js
@@ -7,100 +7,84 @@ const Op = db.Sequelize.Op;
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.render("index", { title: "Express" });
+  res.render("index", { title: "Express" });
 });
 //login
 
 router.post("/login", function(req, res) {
-    let email = req.body.email;
-    db.User.findOne({
-        where: {
-            email: email
-        }
-    }).then(result => {
-        if (result) {
-            if (passwordHash.verify(req.body.password, result.password)) {
-                let token = jwt.sign({ user_id: result.id, user_name: result.name,user_role:result.role }, "qtahhnmsv");
-                return res.send({
-                    error: false,
-                    data: result,
-                    token: token,
-                    message: "Login success"
-                });
-            } else {
-                return res.send({
-                    error: true,
-                    data: result,
-                    message: "Wrong password, please try again"
-                });
-            }
-        } else {
-            return res.send({ error: true, data: result, message: "Wrong email,please try again" });
-        }
-    });
+  let email = req.body.email;
+  db.User.findOne({
+    where: {
+      email: email
+    }
+  }).then(result => {
+    if (result) {
+      if (passwordHash.verify(req.body.password, result.password)) {
+        let token = jwt.sign(
+          {
+            user_id: result.id,
+            user_name: result.name,
+            user_role: result.role
+          },
+          "qtahhnmsv"
+        );
+        return res.send({
+          error: false,
+          data: result,
+          token: token,
+          message: "Login success"
+        });
+      } else {
+        return res.send({
+          error: true,
+          data: result,
+          message: "Wrong password, please try again"
+        });
+      }
+    } else {
+      return res.send({
+        error: true,
+        data: result,
+        message: "Wrong email,please try again"
+      });
+    }
+  });
 });
 
 //register
 
 router.post("/register", function(req, res) {
-    let user = req.body;
-    user.password = passwordHash.generate(user.password);
-    if (!user) {
-        return res
-            .status(400)
-            .send({ error: true, message: "Please provide blog" });
+  let user = req.body;
+  user.password = passwordHash.generate(user.password);
+  if (!user) {
+    return res
+      .status(400)
+      .send({ error: true, message: "Please provide blog" });
+  }
+  db.User.findOne({
+    where: {
+      email: user.email
     }
-    db.User.findOne({
-        where: {
-            email: user.email
-        }
-    }).then(exist => {
-        if (exist) {
-            return res.send({
-                error: true,
-                message: " Email existed, please try another one"
-            });
-        } else {
-            db.User.create(user).then(result => {
-                return res.send({
-                    error: false,
-                    data: result,
-                    message: "Registion success"
-                });
-            });
-        }
-    });
+  }).then(exist => {
+    if (exist) {
+      return res.send({
+        error: true,
+        message: " Email existed, please try another one"
+      });
+    } else {
+      db.User.create(user).then(result => {
+        return res.send({
+          error: false,
+          data: result,
+          message: "Registion success"
+        });
+      });
+    }
+  });
 });
 
-//register
-
-router.post("/register", function(req, res) {
-    let user = req.body;
-    user.password = passwordHash.generate(user.password);
-    if (!user) {
-        return res
-            .status(400)
-            .send({ error: true, message: "Please provide blog" });
-    }
-    db.User.findOne({
-        where: {
-            email: user.email
-        }
-    }).then(exist => {
-        if (exist) {
-            return res.send({
-                error: true,
-                message: " Email existed, please try another one"
-            });
-        } else {
-            db.User.create(user).then(result => {
-                return res.send({
-                    error: false,
-                    data: result,
-                    message: "Registion success"
-                });
-            });
-        }
-    });
+//decoded
+router.get("/get_user", function(req, res, next) {
+  console.log("req.decoded");
 });
 module.exports = router;
