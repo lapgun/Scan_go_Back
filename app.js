73f5f7370813f1800796abdd2584a473753ea415
lapgun <lapgun06@gmail.com>
2019-12-12 15:35:57 +0700

    login fb

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ var jwt = require('jsonwebtoken');
 var app = express();
 
 // view engine setup
-app.use(cors());
+app.use(cors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(logger("dev"));
--- routes/loginFacebook.js
+++ routes/loginFacebook.js
@@ -14,28 +14,24 @@ app.use(session(
     }
 ));
 app.use(passport.session());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000/social/auth/fb");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 router.get('/', (req, res) => {
     console.log("ádsds");
 });
-router.get('/auth/fb', function(req,res) {
+router.get('/auth/fb', passport.authenticate('facebook', {scope: "email"}));
 
-    console.log(passport.authenticate('facebook', {scope: "email"}));
-
+router.get('/auth/fb/cb', passport.authenticate('facebook', {successRedirect: '/account', failureRedirect: '/'}),function (req,res) {
+        if(req.user){
+            return res.redirect(303, '/#!/fb/');
+        } else {
+            return res.status(400).json({message:"Not found"});
+        }
 });
-
-router.get('/auth/fb/cb', passport.authenticate('facebook', { successRedirect: '/account', failureRedirect: '/' }));
 
 router.use('/auth/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 passport.use(new passportfb({
-
         clientID: "373882913288063",
         clientSecret: "2766e871dd19193d2231285bdcceb433",
         callbackURL: "http://localhost:4000/auth/fb/cb",
@@ -46,10 +42,10 @@ passport.use(new passportfb({
         console.log(profile);
     }
 ));
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
     cb(null, user);
 });
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
 });
 module.exports = router;
