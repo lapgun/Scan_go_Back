98fe1fc5a9c542f17ddb5c5dd5bc261aa1bac482
ViVu1002 <nguyenthila100221@gmail.com>
2019-12-18 13:03:43 +0700

    text

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ var db = require("../models");
 const Op = db.Sequelize.Op;
 
 // get by products Id
-router.post("/get/", function (req, res) {
+router.post("/get/", function(req, res) {
     let currentPage = req.body.currentPage ? parseInt(req.body.currentPage) : 1;
     let perPage = req.body.perPage ? parseInt(req.body.perPage) : 3;
     db.Comment.findAndCountAll({
@@ -32,7 +32,7 @@ router.post("/get/", function (req, res) {
     });
 });
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
     let currentPage = req.query.currentPage ? parseInt(req.query.currentPage) : 1;
     let perPage = req.query.perPage ? parseInt(req.query.perPage) : 6;
     db.Comment.findAndCountAll({
@@ -58,8 +58,24 @@ router.get("/", function (req, res) {
     });
 });
 
-// get all by product Id
-router.get("/get/by_product_id/:id", function (req, res) {
+// get search
+
+router.get('/search', function(req, res) {
+        let search = req.query.search;
+        db.Comment.findAndCountAll({
+            where: {
+                name: {
+                    [Op.substring]: search,
+                }
+            }
+
+        }).then(results => {
+            let data = results.rows
+            return res.send({ data: data })
+        })
+    })
+    // get all by product Id
+router.get("/get/by_product_id/:id", function(req, res) {
     db.Comment.findAndCountAll({
         where: {
             productId: req.params.id
@@ -70,14 +86,14 @@ router.get("/get/by_product_id/:id", function (req, res) {
 })
 
 //get by comment id
-router.get("/:id", function (req, res) {
+router.get("/:id", function(req, res) {
     db.Comment.findByPk(req.params.id).then(result => {
         return res.send(result);
     });
 });
 
 //Post
-router.post("/", function (req, res) {
+router.post("/", function(req, res) {
     let comment = req.body;
     db.Comment.create(comment).then(result => {
         return res.send(result);
@@ -86,7 +102,7 @@ router.post("/", function (req, res) {
 //post pagination
 
 //Update
-router.put("/:id", function (req, res) {
+router.put("/:id", function(req, res) {
     let comment_id = req.body.id;
     let comment = req.body;
     db.Comment.update(comment, {
@@ -99,7 +115,7 @@ router.put("/:id", function (req, res) {
 });
 
 //Delete
-router.delete("/:id", function (req, res) {
+router.delete("/:id", function(req, res) {
     let comment_id = req.params.id;
 
     if (!comment_id) {
