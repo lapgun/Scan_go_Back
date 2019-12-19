4192070d89ebd3b7117e66369ae592dafd56d560
ViVu1002 <nguyenthila100221@gmail.com>
2019-12-19 17:46:00 +0700

    text

--- migrations/20191205020302-create-comments.js
+++ migrations/20191205020302-create-comments.js
@@ -1,43 +1,36 @@
 "use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Comments", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      comment: {
-        allowNull: false,
-        notNull: true,
-        type: Sequelize.STRING
-      },
-      img: {
-        type: Sequelize.STRING
-      },
-      productId: {
-        allowNull: false,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER
-      },
-      rating: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Comments");
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Comments", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            comment: {
+                allowNull: false,
+                notNull: true,
+                type: Sequelize.STRING
+            },
+            userId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            rating: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Comments");
+    }
 };
--- models/comment.js
+++ models/comment.js
@@ -1,41 +1,37 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define(
-    "Comment",
-    {
-      comment: {
-        type: DataTypes.STRING,
-        validate: {
-          len: [2, 255]
-        }
-      },
-      img: DataTypes.STRING,
-      parentId: DataTypes.INTEGER,
-      userId: DataTypes.INTEGER,
-      rating: {
-        type: DataTypes.INTEGER,
-        validate: {
-          min: 1,
-          max: 5,
-          isNumeric: true
-        }
-      },
-      name: DataTypes.STRING,
-      productId: DataTypes.INTEGER
-    },
-    {}
-  );
-  Comment.associate = function (models) {
-    Comment.belongsTo(models.User, {
-      foreignKey: "userId",
-      targetKey: "id",
-      as: "user"
-    });
-    Comment.belongsTo(models.Products, {
-      foreignKey: "productId",
-      targetKey: "id",
-      as: "product"
-    })
-  };
-  return Comment;
+    const Comment = sequelize.define(
+        "Comment", {
+            comment: {
+                type: DataTypes.STRING,
+                validate: {
+                    len: [2, 255]
+                }
+            },
+            userId: DataTypes.INTEGER,
+            rating: {
+                type: DataTypes.INTEGER,
+                validate: {
+                    min: 1,
+                    max: 5,
+                    isNumeric: true
+                }
+            },
+            name: DataTypes.STRING,
+            productId: DataTypes.INTEGER
+        }, {}
+    );
+    Comment.associate = function(models) {
+        Comment.belongsTo(models.User, {
+            foreignKey: "userId",
+            targetKey: "id",
+            as: "user"
+        });
+        Comment.belongsTo(models.Products, {
+            foreignKey: "productId",
+            targetKey: "id",
+            as: "product"
+        })
+    };
+    return Comment;
 };
